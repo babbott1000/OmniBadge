d333db6726fd1b85bcc9a94a8f557a3d0e214aa4
Jack <jackbaude@gmail.com>
2019-03-06 14:42:27 -0600

    test test tes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "bootstrap/dist/css/bootstrap.css";
+
 class Layout extends React.Component {
   render() {
     return (
 		<p style={{"font-size": "128px"}}>
-
+            TEST TEST TEST
 		</p>
     );
   }
