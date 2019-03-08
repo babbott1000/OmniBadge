2d2b673d9814fa3b8c157812fe1364e0716fc0b1
Ben <babbott1000@gmail.com>
2019-03-07 23:45:39 -0600

    Completed HTTPS Setup

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-const express = require('express');
-const app = express();
+'use strict';
 
-const port = 80;
+function approveDomain(opts, certs, cb) {
 
-app.use('/', express.static(__dirname + '/client'));
+  opts.email = 'flashpassedu@gmail.com';
+  opts.agreeTos = true;
+  opts.domains = [ 'www.flashpassedu.com', 'flashpassedu.com' ];
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + "/client/index.html");
+  cb(null, { options: opts, certs: certs });
+}
+
+var greenlock = require('greenlock-express').create({
+
+  // Let's Encrypt v2 is ACME draft 11
+  version: 'draft-11'
+
+, server: 'https://acme-v02.api.letsencrypt.org/directory'
+  // Note: If at first you don't succeed, stop and switch to staging
+  // https://acme-staging-v02.api.letsencrypt.org/directory
+
+//[ 'flashpassedu.com', 'www.flashpassedu.com' ]
+, approveDomains: function (opts, certs, cb) {
+  approveDomain(opts, certs, cb);
+}
+
+, configDir: 'C:/Users/Ben/acme'
+
+, app: function (req, res) {
+  require('./server.js')(req, res);
+}
+
+
 });
 
-app.listen(port);
+var server = greenlock.listen(80, 443);
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = 80;
+
+app.use('/', express.static(__dirname + '/client'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + "/client/index.html");
+});
+
+module.exports = app;
+
+if (require.main === module) {
+	app.listen(port);
+}
