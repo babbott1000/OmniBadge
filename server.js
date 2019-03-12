229988313c63598f2256479344f8c120f35244c5
Ben <babbott1000@gmail.com>
2019-03-11 21:57:37 -0500

    Installed DB Packages, Added Favicon

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const config = require('E:/config.js');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const port = 80;
+
+mongoose.connect(config.database, { useNewUrlParser: true });
+app.set('superSecret', config.secret);
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 app.use('/', express.static(__dirname + '/client'));
 
