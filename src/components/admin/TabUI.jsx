f1f52f475e86c37fbe894aa198bd76570e4cbf6e
Jack <jackbaude@gmail.com>
2019-04-16 08:29:07 -0500

    will now compile - fixed import errors

--- src/components/admin/TabUI.jsx
+++ src/components/admin/TabUI.jsx
@@ -6,8 +6,8 @@ import React from 'react';
 
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from "react-bootstrap/Tab";
-import { Students } from "./Students.jsx";
-import { History } from "./History.jsx";
+import { Students } from "./students.jsx";
+import { History } from "./history.jsx";
 
 export class TabUI extends React.Component{
 
