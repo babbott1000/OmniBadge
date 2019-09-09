4a2e48e3306c3ede9ba0352ababde148ab3ec180
Jack <jackbaude@gmail.com>
2019-09-09 17:31:20 -0500

    Boiler plate for the about page

--- src/components/about/thePeople.jsx
+++ src/components/about/thePeople.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export class About_us extends React.Component {
+export class ThePeople extends React.Component {
 
     render() {
         return (
             <div>
-                <h1> HAHAHAH</h1>
+                <p> </p>
             </div>
         );
     }
---
+++ src/components/about/theProject.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class TheProject extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p> hey look no more vapers = bach very happy :')</p>
+            </div>
+        );
+    }
+}
