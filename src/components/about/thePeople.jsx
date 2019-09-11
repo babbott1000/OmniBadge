e82e3a56665daa268a98f6226561c6b3af961d73
Jack <Jackbaude@gmail.com>
2019-09-10 21:34:44 -0500

    Wrote the about section about who and what we are

--- src/components/about/thePeople.jsx
+++ src/components/about/thePeople.jsx
@@ -5,7 +5,7 @@ export class ThePeople extends React.Component {
     render() {
         return (
             <div>
-                <p className="abouttext"> Ben and i made this website and it is sure to break so yeah thats kinda cool ig </p>
+                <p className="abouttext"> Ben and Jack are the main devlopers behind the Byte Pass project. Ben manages server end devlopment while Jack handles Front end development. Ben and Jack attend Stillwater Area High school as graduates of 21'. Both enjoy coding in a multitude of and are passionate about computer science.  </p>
             </div>
         );
     }
--- src/components/about/theProject.jsx
+++ src/components/about/theProject.jsx
@@ -5,7 +5,7 @@ export class TheProject extends React.Component {
     render() {
         return (
             <div>
-                <p className="abouttext"> hey look no more vapers = bach very happy :')</p>
+                <p className="abouttext"> Byte Pass is a fast hall pass system developed at Stillwater Area High school. The pass system includes a physical pass  for all students along with a scanner for each class, and a live view for teachers. The project goals are aimed at reliable, fast and safe pass exchanges that solves many of hall pass issues. </p>
             </div>
         );
     }
